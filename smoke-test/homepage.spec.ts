0cca23ce238f066100da9ef2ca89cc92ebb13f68
Gerrit Watermann <g.watermann2@gmx.de>
2026-06-10 19:06:47 +0200

    fix(frontend): reformat

--- smoke-test/homepage.spec.ts
+++ smoke-test/homepage.spec.ts
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('homepage loads', async ({ page }) => {
-  await page.goto('http://127.0.0.1:4173');
+test("homepage loads", async ({ page }) => {
+  await page.goto("http://127.0.0.1:4173");
 
   await expect(page).toHaveTitle(/./);
 });
